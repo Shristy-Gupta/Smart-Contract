be6fc93dfcaf78c451cf9281e658e597d12e2414
Shristy Gupta <shristysgupta@gmail.com>
2021-07-15 00:41:57 +0530

    Create StartStopUpdateExample.sol

---
+++ StartStopUpdateExample.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.1;
+
+contract StartStopUpdateExample {
+    address owner;
+    constructor() public{
+        owner=msg.sender;
+    }
+    function sendMoney() public payable {
+
+    }
+
+    function withdrawAllMoney(address payable _to) public {
+        require(msg.sender==owner,"You are not the owner");
+        _to.transfer(address(this).balance);
+    }
+    function destroySmartContract(address payable _to) public{
+       require(msg.sender==owner,"You are not the owner"); 
+        selfdestruct(_to);
+    }
+}
