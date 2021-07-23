47e509fac08b091cd23ff6bb61c1e22df498cb27
Shristy Gupta <shristysgupta@gmail.com>
2021-07-23 16:17:51 +0530

    Update SharedWallet.sol

--- Shared Wallet/SharedWallet.sol
+++ Shared Wallet/SharedWallet.sol
@@ -6,18 +6,21 @@ contract SimpleWallet is Allowance{
   
     event MoneySent(address indexed _beneficiary, uint _amount);
     event MoneyReceived(address indexed _from, uint _amount);
+    uint public _balanceamount;
     function withdrawMoney(address payable _to, uint _amount) public ownerOrAllowed(_amount){
         require(_amount<=address(this).balance,"Insufficient Balance");
         if(owner()!=msg.sender){
             reduceAllowance(msg.sender,_amount);
         }
-        emit MoneySent(_to,_amount);    
+        emit MoneySent(_to,_amount);
         _to.transfer(_amount);
+         _balanceamount=address(this).balance;
     }
     function renounceOwnership() public override onlyOwner{
         revert("Cannot renounce ownership!");
     }
     receive () external payable{
+        _balanceamount=msg.value;
         emit MoneyReceived(msg.sender,msg.value);
     }
 }
